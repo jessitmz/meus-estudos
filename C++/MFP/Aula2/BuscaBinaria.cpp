f2533b1dbbe196c20c5b22d10e3a3749754ce374
Jessica <jeeh.tmz@gmail.com>
2026-04-24 20:32:29 -0300

    consegui ajeitar meu códido de busca binária

--- C++/MFP/Aula2/BuscaBinaria.cpp
+++ C++/MFP/Aula2/BuscaBinaria.cpp
@@ -22,22 +22,41 @@ int32_t main() {
 
         bool ok = 0;
         int l = 0, r = n-1;
-
+        cout << "valor de L no começo da busca: " << l << endl;
+        cout << "valor de R no começo da busca: " << r << endl;
+        
         while(l <= r) {
+            
+            cout << "valor de L agora é: " << l << endl;
+            cout << "valor de R agora é: " << r << endl;
 
-            int mid = (1 + r)/2;
+            int mid = (l + r)/2;
+            cout << "O mid agora é: " << mid << endl;
 
+            cout << "O valor de v[mid] é: " << v[mid] << endl;
             if(v[mid] == x) {
+                cout << "entrou no if" << endl;
                 ok = 1;
                 break;
             }
-            else if(v[mid] > x) r = mid - 1;
-            else l = mid + 1;
+            else if(v[mid] > x) {
+                cout << "entrou no else if" << endl;
+                r = mid - 1;
+            }
+            else {
+                cout << "entrou no else" << endl;
+                l = mid + 1;
+            }
 
         }
 
-        if(ok) cout << "Achou!\n";
-        else cout << "Não achou.\n";
+        if(ok) {
+            cout << "Achou!\n";
+        }
+
+        else {
+            cout << "Não achou.\n";
+        }
 
     }
 
